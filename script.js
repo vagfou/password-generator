97b1a51ad8f89f323fd61f5d16299cd92160bcfe
vagfou <foufoulas.evangelos@protonmail.com>
2021-04-21 19:28:21 +0300

    getRandomsymbol implemented

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    Symbol: getRandomSymbol
+}
+
 function getRandomLower () {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
@@ -10,4 +17,8 @@ function getRandomNumber () {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
 
-console.log(getRandomNumber())
+function getRandomSymbol () {
+    const symbols = '!@#$%^&*(){}[]=<>/,.'
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
